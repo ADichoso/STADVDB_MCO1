4e6adcdeb298eaf81ec946321d160c1e3ab66067
kenStinks <john_kirsten_espiritu@dlsu.edu.ph>
2024-02-18 13:08:30 +0800

    scrollbar added

--- gui.py
+++ gui.py
@@ -52,41 +52,79 @@ figure_list = [
 #GUI
 root = tk.Tk()
 root.title('OLAP Application')
+root.geometry("800x450")
+root.pack_propagate(0)
 
-side_frame = tk.Frame(root, padx=5, pady=5)
+main_frame = tk.Frame(root)
+main_frame.pack(fill="both", expand=1)
+
+main_canvas = tk.Canvas(main_frame)
+main_canvas.pack(side="right", fill="both", expand=1)
+
+side_frame = tk.Frame(main_frame, padx=5, pady=5)
 side_frame.pack(side="left", padx=5,pady=5, fill="y")
 
-canvas1 = FigureCanvasTkAgg(fig1, root)
+scrollbar = tk.Scrollbar(main_frame, orient='vertical', command=main_canvas.yview)
+scrollbar.pack(side="right", fill='y')
 
-def button_update(image_number, refinement_number):
+main_canvas.configure(yscrollcommand=scrollbar.set)
+main_canvas.bind('<Configure>', lambda e: main_canvas.configure(scrollregion=main_canvas.bbox("all")))
+
+second_frame = tk.Frame(main_canvas)
+canvas1 = FigureCanvasTkAgg(fig1, second_frame)
+canvas2 = tk.Frame(second_frame, padx=5, pady=5)
+canvas2.pack(side="left", padx=5,pady=5, fill="y")
+
+main_canvas.create_window((0,0), window=second_frame, anchor="nw")
+
+def button_update(image_number, refinement_number, view_type):
     global canvas1
+    global canvas2
     global side_frame
 
     image_number = image_number % len(figure_list)
     
-    side_frame.grid_forget()
     for child in side_frame.winfo_children():
         child.destroy()
 
-    canvas1.get_tk_widget().pack_forget()
-    canvas1 = FigureCanvasTkAgg(figure_list[image_number][refinement_number], root)
-    canvas1.draw()
-    canvas1.get_tk_widget().pack(side="right", fill="both", expand=True)
+    for child in canvas2.winfo_children():
+        child.destroy()
     
-    button_back = tk.Button(side_frame, text = "<<", command=lambda: button_update(image_number-1, 0), width=5)
+    canvas2.pack_forget()
+    canvas2 = tk.Frame(second_frame, padx=5, pady=5)
+    canvas2.pack(side="left", padx=5,pady=5, fill="y")
+
+    canvas1.get_tk_widget().pack_forget()
+    
+    if view_type:
+        curr_df = df_list[image_number][refinement_number]
+        for j in range(len(curr_df.columns)):
+            tk.Label(text=curr_df.columns[j].replace('_', ' ').title(), master=canvas2, font='Helvetica 10 bold').grid(row=0,column=j, sticky="W", padx=10, pady=5)
+            
+            for i in range(len(curr_df)):
+                tk.Label(text=curr_df.iloc[i,j], master=canvas2).grid(row=i+1,column=j, sticky="W", padx=10)
+    else:
+        canvas1 = FigureCanvasTkAgg(figure_list[image_number][refinement_number], second_frame)
+        canvas1.draw()
+        canvas1.get_tk_widget().pack(side="left")
+        
+    
+    button_back = tk.Button(side_frame, text = "<<", command=lambda: button_update(image_number-1, 0, view_type), width=5)
     curr_fig = tk.Label(text="Fig: "+str(image_number+1), master=side_frame, width=5, pady=20)
     curr_ref = tk.Label(text="Ref: "+chr(ord('`')+refinement_number+1), master=side_frame, width=5)
-    button_next = tk.Button(side_frame, text = ">>", command=lambda: button_update(image_number+1, 0), width=5)
+    button_next = tk.Button(side_frame, text = ">>", command=lambda: button_update(image_number+1, 0, view_type), width=5)
+    table_view = tk.Button(side_frame, text = "<o>", command=lambda: button_update(image_number, 0, not view_type), width=5)
 
+    table_view.grid(row=1,column=0)
     button_back.grid(row=0, column=0)
     curr_fig.grid(row=0, column=1)
     curr_ref.grid(row=1, column=1)
     button_next.grid(row=0, column=2)
 
     for i in range(len(figure_list[image_number])):
-        tk.Button(side_frame, text = chr(ord('`')+i+1), command=lambda i=i: button_update(image_number, i), width=8).grid(row=i+2,column=1, pady=2)
+        tk.Button(side_frame, text = chr(ord('`')+i+1), command=lambda i=i: button_update(image_number, i, view_type), width=8).grid(row=i+2,column=1, pady=2)
 
     
 
-button_update(0, 0)
+button_update(0, 0, False)
 root.mainloop()
--- gui2.py
+++ gui2.py
@@ -1,3 +1,5 @@
+_DEBUG = True #skip connecting to the database
+
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
@@ -25,21 +27,22 @@ def connectToDatabase(query):
 # Chart 1: Get yearly count of appointments, grouped into virtual and non-virtual appointments, for each city (roll-up & drill down) #
 ######################################################################################################################################
 
-query1 = "SELECT c.region_name, c.province, c.city, YEAR(a.queuedate) AS appointment_year, SUM(CASE WHEN a.isvirtual = 1 THEN 1 ELSE 0 END) AS virtual_appointments, SUM(CASE WHEN a.isvirtual = 0 THEN 1 ELSE 0 END) AS non_virtual_appointments FROM appointments a JOIN clinics c ON a.clinicid = c.clinicid WHERE a.apptstatus = \"COMPLETE\" AND NOT YEAR(a.queuedate)=1970 GROUP BY c.region_name, c.province, c.city, YEAR(a.queuedate) WITH ROLLUP ORDER BY c.region_name, c.province, c.city, appointment_year;"
-query2 = "SELECT d.mainspecialty AS specialty, YEAR(a.starttime) AS appointment_year, MONTH(a.starttime) AS appointment_month, COUNT(*) AS total_appointments FROM appointments a JOIN doctors d ON a.doctorid = d.doctorid WHERE NOT YEAR(a.starttime) = 1970 GROUP BY d.mainspecialty, YEAR(a.starttime), MONTH(a.starttime) WITH ROLLUP ORDER BY d.mainspecialty, YEAR(a.starttime), MONTH(a.starttime);"
-query3 = "SELECT DAYOFWEEK(apptdate) AS \"day_of_appointment\", D.city, D.province, D.region_name, AVG(D.appts) FROM (SELECT c.city, c.province, c.region_name, DATE(a.queuedate) as apptdate, COUNT(a.apptid) AS appts FROM appointments a JOIN clinics c ON a.clinicid = c.clinicid WHERE YEAR(a.queuedate) <> 1970 GROUP BY DATE(a.queuedate), c.city, c.province, c.region_name) D GROUP BY DAYOFWEEK(apptdate), D.city, D.province, D.region_name WITH ROLLUP ORDER BY DAYOFWEEK(apptdate), AVG(D.appts) DESC, D.city, D.province, D.region_name;"
-query4 = "SELECT d.mainspecialty, p.agerange, p.age, a.isvirtual, COUNT(a.apptid) AS count_appointments, AVG(TIMESTAMPDIFF(MINUTE, a.starttime, a.endtime)) AS average_appointment_duration_minutes FROM appointments a JOIN patients p ON a.pxid = p.pxid JOIN doctors d ON a.doctorid = d.doctorid WHERE YEAR(a.starttime) <> 1970 AND YEAR(a.endtime) <> 1970 AND a.apptstatus = \"COMPLETE\" AND p.agerange <> \"INVALID\" GROUP BY d.mainspecialty, p.agerange, p.age, a.isvirtual WITH ROLLUP ORDER BY d.mainspecialty, p.agerange, p.age, a.isvirtual;"
-query5 = "SELECT c.region_name, c.province, c.city, p.agerange, p.age, d.mainspecialty, COUNT(*) as total_appointments FROM appointments a JOIN patients p ON a.pxid  = p.pxid JOIN  doctors d ON a.doctorid = d.doctorid JOIN clinics c ON a.clinicid = c.clinicid GROUP BY c.region_name, c.province, c.city, p.agerange, p.age, d.mainspecialty  WITH ROLLUP ORDER BY c.region_name, c.province, c.city, p.agerange, p.age, d.mainspecialty;"
-query1_df = connectToDatabase(query1)
-query2_df = connectToDatabase(query2)
-query3_df = connectToDatabase(query3)
-query4_df = connectToDatabase(query4)
-query5_df = connectToDatabase(query5)
-print(query1_df)
-print(query2_df)
-print(query3_df)
-print(query4_df)
-print(query5_df)
+if (not _DEBUG):
+    query1 = "SELECT c.region_name, c.province, c.city, YEAR(a.queuedate) AS appointment_year, SUM(CASE WHEN a.isvirtual = 1 THEN 1 ELSE 0 END) AS virtual_appointments, SUM(CASE WHEN a.isvirtual = 0 THEN 1 ELSE 0 END) AS non_virtual_appointments FROM appointments a JOIN clinics c ON a.clinicid = c.clinicid WHERE a.apptstatus = \"COMPLETE\" AND NOT YEAR(a.queuedate)=1970 GROUP BY c.region_name, c.province, c.city, YEAR(a.queuedate) WITH ROLLUP ORDER BY c.region_name, c.province, c.city, appointment_year;"
+    query2 = "SELECT d.mainspecialty AS specialty, YEAR(a.starttime) AS appointment_year, MONTH(a.starttime) AS appointment_month, COUNT(*) AS total_appointments FROM appointments a JOIN doctors d ON a.doctorid = d.doctorid WHERE NOT YEAR(a.starttime) = 1970 GROUP BY d.mainspecialty, YEAR(a.starttime), MONTH(a.starttime) WITH ROLLUP ORDER BY d.mainspecialty, YEAR(a.starttime), MONTH(a.starttime);"
+    query3 = "SELECT DAYOFWEEK(apptdate) AS \"day_of_appointment\", D.city, D.province, D.region_name, AVG(D.appts) FROM (SELECT c.city, c.province, c.region_name, DATE(a.queuedate) as apptdate, COUNT(a.apptid) AS appts FROM appointments a JOIN clinics c ON a.clinicid = c.clinicid WHERE YEAR(a.queuedate) <> 1970 GROUP BY DATE(a.queuedate), c.city, c.province, c.region_name) D GROUP BY DAYOFWEEK(apptdate), D.city, D.province, D.region_name WITH ROLLUP ORDER BY DAYOFWEEK(apptdate), AVG(D.appts) DESC, D.city, D.province, D.region_name;"
+    query4 = "SELECT d.mainspecialty, p.agerange, p.age, a.isvirtual, COUNT(a.apptid) AS count_appointments, AVG(TIMESTAMPDIFF(MINUTE, a.starttime, a.endtime)) AS average_appointment_duration_minutes FROM appointments a JOIN patients p ON a.pxid = p.pxid JOIN doctors d ON a.doctorid = d.doctorid WHERE YEAR(a.starttime) <> 1970 AND YEAR(a.endtime) <> 1970 AND a.apptstatus = \"COMPLETE\" AND p.agerange <> \"INVALID\" GROUP BY d.mainspecialty, p.agerange, p.age, a.isvirtual WITH ROLLUP ORDER BY d.mainspecialty, p.agerange, p.age, a.isvirtual;"
+    query5 = "SELECT c.region_name, c.province, c.city, p.agerange, p.age, d.mainspecialty, COUNT(*) as total_appointments FROM appointments a JOIN patients p ON a.pxid  = p.pxid JOIN  doctors d ON a.doctorid = d.doctorid JOIN clinics c ON a.clinicid = c.clinicid GROUP BY c.region_name, c.province, c.city, p.agerange, p.age, d.mainspecialty  WITH ROLLUP ORDER BY c.region_name, c.province, c.city, p.agerange, p.age, d.mainspecialty;"
+    query1_df = connectToDatabase(query1)
+    query2_df = connectToDatabase(query2)
+    query3_df = connectToDatabase(query3)
+    query4_df = connectToDatabase(query4)
+    query5_df = connectToDatabase(query5)
+    print(query1_df)
+    print(query2_df)
+    print(query3_df)
+    print(query4_df)
+    print(query5_df)
 
 df1 = pd.read_csv("datasets/number1/1.csv")
 #group data by city
@@ -336,12 +339,27 @@ root.title('OLAP Application')
 root.geometry("800x450")
 root.pack_propagate(0)
 
-side_frame = tk.Frame(root, padx=5, pady=5)
+main_frame = tk.Frame(root)
+main_frame.pack(fill="both", expand=1)
+
+main_canvas = tk.Canvas(main_frame)
+main_canvas.pack(side="right", fill="both", expand=1)
+
+side_frame = tk.Frame(main_frame, padx=5, pady=5)
 side_frame.pack(side="left", padx=5,pady=5, fill="y")
 
-canvas1 = FigureCanvasTkAgg(fig1, root)
-canvas2 = tk.Frame(root, padx=5, pady=5)
+scrollbar = tk.Scrollbar(main_frame, orient='vertical', command=main_canvas.yview)
+scrollbar.pack(side="right", fill='y')
+
+main_canvas.configure(yscrollcommand=scrollbar.set)
+main_canvas.bind('<Configure>', lambda e: main_canvas.configure(scrollregion=main_canvas.bbox("all")))
+
+second_frame = tk.Frame(main_canvas)
+canvas1 = FigureCanvasTkAgg(fig1, second_frame)
+canvas2 = tk.Frame(second_frame, padx=5, pady=5)
 canvas2.pack(side="left", padx=5,pady=5, fill="y")
+
+main_canvas.create_window((0,0), window=second_frame, anchor="nw")
 
 def button_update(image_number, refinement_number, view_type):
     global canvas1
@@ -357,7 +375,7 @@ def button_update(image_number, refinement_number, view_type):
         child.destroy()
     
     canvas2.pack_forget()
-    canvas2 = tk.Frame(root, padx=5, pady=5)
+    canvas2 = tk.Frame(second_frame, padx=5, pady=5)
     canvas2.pack(side="left", padx=5,pady=5, fill="y")
 
     canvas1.get_tk_widget().pack_forget()
@@ -370,9 +388,9 @@ def button_update(image_number, refinement_number, view_type):
             for i in range(len(curr_df)):
                 tk.Label(text=curr_df.iloc[i,j], master=canvas2).grid(row=i+1,column=j, sticky="W", padx=10)
     else:
-        canvas1 = FigureCanvasTkAgg(figure_list[image_number][refinement_number], root)
+        canvas1 = FigureCanvasTkAgg(figure_list[image_number][refinement_number], second_frame)
         canvas1.draw()
-        canvas1.get_tk_widget().pack(side="right", fill="both", expand=True)
+        canvas1.get_tk_widget().pack(side="left")
         
     
     button_back = tk.Button(side_frame, text = "<<", command=lambda: button_update(image_number-1, 0, view_type), width=5)
